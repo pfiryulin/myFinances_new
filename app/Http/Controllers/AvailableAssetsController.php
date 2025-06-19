a8ae6b998b91849a064fe5cc4490067a0ac4d733
pfiryulin <pfirulin@yandex.ru>
2025-06-19 19:46:47 +0800

    создана таблица текусвободных, средств, модель и кÐонтроллер

---
+++ app/Http/Controllers/AvailableAssetsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AvailableAssets;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AvailableAssetsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AvailableAssets $availableAssets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AvailableAssets $availableAssets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AvailableAssets $availableAssets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AvailableAssets $availableAssets)
+    {
+        //
+    }
+}
